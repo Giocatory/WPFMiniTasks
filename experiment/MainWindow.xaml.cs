8faea037e487b14dcb5962ffe0166c72a8ec2571
Giocatory <nivolyas@gmail.com>
2022-02-20 12:56:31 +0300

    Добавьте файлы проекта.

---
+++ experiment/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace experiment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You click to button!");
+        }
+
+        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Title = e.GetPosition(this).ToString();
+        }
+    }
+}
