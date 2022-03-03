a30eb230220915c8c175e883ef4257c7e991b819
Giocatory <nivolyas@gmail.com>
2022-03-03 11:04:47 +0300

    Main view model on MainWindow

---
+++ ForExperiments/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ForExperiments.ViewModels.Base;
+
+namespace ForExperiments.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок Окна
+        private string _Title = "Mini Apps";
+
+        /*
+         set { 
+            if (Equals(_Title, value)) return;
+            _Title = value;
+            OnPropertyChanged();
+        // OR
+            Set(ref _Title, value);
+        }
+        */
+        /// <summary> Заголовок Окна </summary>
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+        #endregion
+    }
+}
