68414b7794a420ac36e109607fc360c89d5ae5d2
Giocatory <nivolyas@gmail.com>
2022-03-06 20:38:19 +0300

    Продолжаем фиксить окно регистрации

--- ForExperiments/ViewModels/RegWindowViewModel.cs
+++ ForExperiments/ViewModels/RegWindowViewModel.cs
@@ -8,11 +8,14 @@ namespace ForExperiments.ViewModels
 
         private string _Title = "Registration form";
         private string _Description = "1) Вводите пожалуйста латинские символы\n2) Пароль должен содержать:\n\t- Большие и маленькие буквы\n\t- Цифры";
+        private string _DescriptionFinall = "- Для завршения нажмите \"Завершить\"\n- Если сомневаетесь, вернитесь назад\n\n\tСпасибо за регистрацию!";
 
         /// <summary> Заголовок Окна </summary>
         public string Title { get => _Title; set => Set(ref _Title, value); }
 
         public string Description { get => _Description; set => Set(ref _Description, value); }
+
+        public string DescriptionFinall { get => _DescriptionFinall; set => Set(ref _DescriptionFinall, value); }
 
         #endregion Заголовок Окна
     }
--- ForExperiments/Views/Windows/RegWindow.xaml.cs
+++ ForExperiments/Views/Windows/RegWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -15,6 +16,67 @@ namespace ForExperiments
         {
             InitializeComponent();
             this.Background = new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), @$"{Environment.CurrentDirectory}\mainBg.jpg")));
+        }
+
+        private void aboutProgram(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Программа предназначена для группировки мини приложений, созданных в процессе обучения WPF. Регистрироваться не обязательно, можно просто зайти, как гость!");
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NextTabIndex(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == "" || Password.Text == "")
+            {
+                MessageBox.Show("Не корректный ввод"); return;
+            }
+            About.Focus();
+        }
+
+        private void PrewTabIndex(object sender, RoutedEventArgs e)
+        {
+            LogPass.Focus();
+        }
+
+        // hover in button
+        private void HoverInButton(object sender, MouseEventArgs e)
+        {
+            ButtonNext.Foreground = Brushes.Blue;
+            ButtonNext.Background = Brushes.Yellow;
+        }
+
+        private void HoverOutButton(object sender, MouseEventArgs e)
+        {
+            ButtonNext.Foreground = Brushes.Yellow;
+            ButtonNext.Background = Brushes.Blue;
+        }
+
+        private void HoverInPrewButton(object sender, MouseEventArgs e)
+        {
+            ButtonPrew.Foreground = Brushes.Blue;
+            ButtonPrew.Background = Brushes.Yellow;
+        }
+
+        private void HoverOutPrewButton(object sender, MouseEventArgs e)
+        {
+            ButtonPrew.Foreground = Brushes.Yellow;
+            ButtonPrew.Background = Brushes.Blue;
+        }
+
+        private void HoverInFinButton(object sender, MouseEventArgs e)
+        {
+            ButtonFinall.Foreground = Brushes.Blue;
+            ButtonFinall.Background = Brushes.Yellow;
+        }
+
+        private void HoverOutFinButton(object sender, MouseEventArgs e)
+        {
+            ButtonFinall.Foreground = Brushes.Yellow;
+            ButtonFinall.Background = Brushes.Blue;
         }
     }
 }
