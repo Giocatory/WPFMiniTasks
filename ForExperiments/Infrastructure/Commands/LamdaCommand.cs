c0d33097804f99028fed283ed263964d397a6525
Giocatory <nivolyas@gmail.com>
2022-03-07 17:43:12 +0300

    Главный класс команд

---
+++ ForExperiments/Infrastructure/Commands/LamdaCommand.cs
@@ -0,0 +1,20 @@
+﻿using ForExperiments.Infrastructure.Commands.Base;
+using System;
+
+namespace ForExperiments.Infrastructure.Commands
+{
+    internal class LamdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LamdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => _Execute(parameter);
+    }
+}
