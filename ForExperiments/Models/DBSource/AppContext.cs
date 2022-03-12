c7f4f66b5a731217660dade137720a857bf15153
Giocatory <nivolyas@gmail.com>
2022-03-12 18:58:27 +0300

    Настройка подкдючения базы данных

---
+++ ForExperiments/Models/DBSource/AppContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace ForExperiments.Models.DBSource
+{
+    internal class AppContext : DbContext
+    {
+        public AppContext() : base("DefaultConnection") { }
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ ForExperiments/Models/DBSource/User.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ForExperiments.Models.DBSource
+{
+    internal class User : INotifyPropertyChanged
+    {
+        private int id;
+        private string login;
+        private string pass;
+        private string mail;
+        private string telegramm;
+        private string mobile;
+
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Login
+        {
+            get => login;
+            set
+            {
+                login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+        public string Pass
+        {
+            get => pass;
+            set
+            {
+                pass = value;
+                OnPropertyChanged("Pass");
+            }
+        }
+        public string Mail
+        {
+            get => mail;
+            set
+            {
+                mail = value;
+                OnPropertyChanged("Mail");
+            }
+        }
+        public string Telegramm
+        {
+            get => telegramm;
+            set
+            {
+                telegramm = value;
+                OnPropertyChanged("Telegramm");
+            }
+        }
+        public string Mobile
+        {
+            get => mobile;
+            set
+            {
+                mobile = value;
+                OnPropertyChanged("Mobile");
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
