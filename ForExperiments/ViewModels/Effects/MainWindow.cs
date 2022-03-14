f23b87a9352a86ceed4f3f6f825ff9ba2eb8e432
Giocatory <nivolyas@gmail.com>
2022-03-14 17:08:39 +0300

    перенос классов

---
+++ ForExperiments/ViewModels/Effects/MainWindow.cs
@@ -0,0 +1,77 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ForExperiments
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region main buttons
+        private void signIn_Click(object sender, RoutedEventArgs e)
+        {
+            AppsList taskWindow = new();
+            if (GuestLogin.IsChecked == true)
+            {
+                taskWindow.ViewModel = $"Добро пожаловать, Guest!\nВыберите что-нибудь из списка приложений!";
+                taskWindow.Show();
+                taskWindow.Owner = this;
+                taskWindow.ShowViewModel();
+                return;
+            }
+            if (login.Text == "" || password.Password == "")
+            {
+                MessageBox.Show("Не корректный ввод логина/пароля");
+                return;
+            }
+            if (login.Text.ToLower() == "admin" && password.Password.ToLower() == "admin")
+            {
+                taskWindow.ViewModel = $"Добро пожаловать, {login.Text}!\nВыберите что-нибудь из списка приложений!";
+                taskWindow.Show();
+                taskWindow.Owner = this;
+                taskWindow.ShowViewModel();
+            }
+            else
+            {
+                MessageBox.Show("База данных еще не подключена!");
+            }
+        }
+
+        private void registration_Click(object sender, RoutedEventArgs e)
+        {
+            RegWindow regWindow = new();
+            regWindow.Show();
+            regWindow.Owner = this;
+        }
+        #endregion main buttons
+
+
+        #region button hover animation
+
+        private void signIn_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            signIn.Background = Brushes.Yellow;
+            signIn.Foreground = Brushes.Blue;
+        }
+
+        private void signIn_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            signIn.Background = Brushes.Blue;
+            signIn.Foreground = Brushes.Yellow;
+        }
+
+        private void reg_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            registration.Background = Brushes.Yellow;
+            registration.Foreground = Brushes.Blue;
+        }
+
+        private void reg_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            registration.Background = Brushes.Blue;
+            registration.Foreground = Brushes.Yellow;
+        }
+        #endregion button hover animation
+    }
+}
--- ForExperiments/Views/Windows/AppsList.xaml.cs
+++ ForExperiments/Views/Windows/AppsList.xaml.cs
@@ -16,6 +16,7 @@ namespace ForExperiments
         {
             InitializeComponent();
             this.Background = new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), @$"{Environment.CurrentDirectory}\img\cofee.jpg")));
+
             ShadowAssist.SetShadowDepth(this, ShadowDepth.Depth0); // для подключения Material Design
 
             #region не прерывный показ текущего времени (внизу окна)
@@ -31,124 +32,5 @@ namespace ForExperiments
 
             #endregion не прерывный показ текущего времени (внизу окна)
         }
-
-        #region Welcome message box
-
-        public string? ViewModel { get; set; }
-
-        public void ShowViewModel()
-        {
-            MessageBox.Show(ViewModel);
-        }
-
-        #endregion Welcome message box
-
-        #region Menu items
-
-        private void MenuItemEnv_Click(object sender, RoutedEventArgs e)
-        {
-            // работа с системными инструментами
-            MessageBox.Show(
-                $"System information:\nUser: {Environment.UserName}\nSystem process: {System.Diagnostics.Process.GetCurrentProcess()}\n" +
-                $"Current directory: {Environment.CurrentDirectory}\nCurrent Managed Thread ID: {Environment.CurrentManagedThreadId}\n" +
-                $"Logical Drives:\n{String.Join('\n', Environment.GetLogicalDrives())}\nhow much processors: {Environment.ProcessorCount}\n" +
-                $"System is 64bit?: {Environment.Is64BitOperatingSystem}\nOS version: {Environment.OSVersion}\nMachine name: {Environment.MachineName}\n" +
-                $".NET version: {Environment.Version}\nSystem directory: {Environment.SystemDirectory}");
-        }
-
-        private void AboutApp_click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Приложение написано на C#10, .NET6, WPF\nПредназначено для личного использования, как показатель процесса обучения...");
-        }
-
-        #endregion Menu items
-
-        #region switch tab Items
-
-        // switch tab Items
-        private void RubTabItemList_click(object sender, RoutedEventArgs e)
-        {
-            TabItemList.Focus();
-            MainWindow.Width = 740;
-        }
-
-        private void RubTabItemTable_click(object sender, RoutedEventArgs e)
-        {
-            TabItemTable.Focus();
-            MainWindow.Width = 400;
-            TableItem.Width = 400;
-        }
-
-        #endregion switch tab Items
-
-        #region hover on list titles
-
-        // mouse enter
-        private void DescriptTitle_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Постарался собрать вопросы, которые могут попадаться на собеседованиях C# программистов, а так же дать подробные ответы.\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void DescriptTitleOne_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Общие вопросы, которые встречаются на собеседованиях, разного уровня и знаний программистов.\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void DescriptTitleTwo_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Чаще всего, встречающиеся вопросы на собеседованиях программистов, уровня Junior (Начинающий)\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void DescriptTitleThree_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Чаще всего, встречающиеся вопросы на собеседованиях программистов, уровня Middle (Уровень знаний по середине, между Junior и Senior)\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void DescriptTitleFour_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Чаще всего, встречающиеся вопросы на собеседованиях программистов, уровня Senior (Опытные программисты)\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void DescriptTitleFive_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Все вопросы с собеседований собраны в один тест, благодаря которому, вы можете проверить свои знания, при подготовке к собеседованию.\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void MiniProjTitle_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Элементарные приложения, которые делают все, во время обучения программированию.\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void MiniProjCalculator_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Приложение - Калькулятор.\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void MiniProjNotebook_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Простейший блокнот.\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void MiniProjCrossesAndZero_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Игра в \"Крестики нолики\"\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void MiniProjPasswordGenerate_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Генератор паролей с разными условиями.\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void MiniProjResumeCreator_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Создатель резюме(Записываете необходимые данные и получаете готовое резюме, по шаблону)\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        private void MiniProjPaint_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            DescriptionText.Text = "Реализация простейшего редактора для рисования.\nЕсли будут предложения и пожелания, напишите мне\ngiocatory@yandex.ru";
-        }
-
-        #endregion hover on list titles
     }
 }
